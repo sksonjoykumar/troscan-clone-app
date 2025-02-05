39f03fffef4c485e37f1cb00b4198f351dabf81f
sksonjoykumar <sksonjoykumar111@gmail.com>
2025-02-05 19:12:38 +0600

    added a feature

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,11 @@
 export default function Home() {
-  
   return (
     <div className="">
       <main>
-        <h1 className="text-4xl text-center mt-4 font-semibold ">
+        <h1 className="mt-4 text-4xl font-semibold text-center text-blue-600">
           Hello World!
         </h1>
       </main>
     </div>
-    
   );
 }
